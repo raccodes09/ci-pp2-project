9d5419205e2332bdf233465f800a3e32899994cc
raccodes09 <>
2023-08-17 20:08:18 +0000

    Add comments to template the javascript script.

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+// get required elements
+
+
+
+
+// declare required variables
+
+
+
+
+
+// question data
+
+
+
+
+// event listener - start button
+
+
+
+
+// function - display questions and options
+
+
+
+
+// function - display result screen
+
+
+
+
+// event listener - start quiz
+
+
+
+
+// event listener - reset button
+
+
+
+
