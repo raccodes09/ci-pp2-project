c8069fe00b230d9898ac72603f94826dc9083668
raccodes09 <>
2023-08-18 17:40:41 +0000

    Add js code, more required elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,14 @@ const startbutton = document.getElementById('startQuiz');
 const usernameInput = document.getElementById('username');
 const quizContainer = document.getElementById('quizContainer');
 const startScreen = document.getElementById('startScreen');
-
-
+const questionScreen = document.getElementById('questionScreen');
+const resultScreen = document.getElementById('resultScreen');
 
 
 // declare required variables
-
-
+let username = '';
+let score = 0;
+let currentQuestion = 0;
 
 
 
